55568a7a34824539a1a9f81d2edd1078ae0c9e45
Jonathan Thom and Tracie Weitzman <jonathan+tracie@>
2017-02-22 13:46:10 -0800

    DB seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+event1 = Event.create({name: "Your biggest city hit by Hurricane Donald! Public emergency declared. City requesting help from Nation."})
+event1.responses.create(description: "winning", capital_effect: -100, population_effect: 0, stability_effect: 0, resources_effect: -100, nation_id: 1)
+event1.responses.create(description: "neutral", capital_effect: -50, population_effect: -250, stability_effect: -0.1, resources_effect: -50, nation_id: 1)
+event1.responses.create(description: "losing", capital_effect: -10, population_effect: -1000, stability_effect: -0.3, resources_effect: 0, nation_id: 1)
+
+event2 = Event.create({name: "Stock Market Crash. Panic reins supreme. Population worries about retirement resources."})
+event2.responses.create(description: "winning", capital_effect: -100, population_effect: 0, stability_effect: 0, resources_effect: -100, nation_id: 1)
+event2.responses.create(description: "neutral", capital_effect: -50, population_effect: -250, stability_effect: -0.1, resources_effect: -50, nation_id: 1)
+event2.responses.create(description: "losing", capital_effect: -10, population_effect: -1000, stability_effect: -0.3, resources_effect: 0, nation_id: 1)
+
+event3 = Event.create({name: "Scientist discover a disease cure for the Donald Trump Virus. Need funding for vaccine distribution."})
+event3.responses.create(description: "winning", capital_effect: -100, population_effect: 0, stability_effect: 0, resources_effect: -100, nation_id: 1)
+event3.responses.create(description: "neutral", capital_effect: -50, population_effect: -250, stability_effect: -0.1, resources_effect: -50, nation_id: 1)
+event3.responses.create(description: "losing", capital_effect: -10, population_effect: -1000, stability_effect: -0.3, resources_effect: 0, nation_id: 1)
+
+event4 = Event.create({name: "New rocket has gone over budget. Local company requests additional government support. Space travel to Mars inspires population."})
+event4.responses.create(description: "winning", capital_effect: -100, population_effect: 0, stability_effect: 0, resources_effect: -100, nation_id: 1)
+event4.responses.create(description: "neutral", capital_effect: -50, population_effect: -250, stability_effect: -0.1, resources_effect: -50, nation_id: 1)
+event4.responses.create(description: "losing", capital_effect: -10, population_effect: -1000, stability_effect: -0.3, resources_effect: 0, nation_id: 1)
