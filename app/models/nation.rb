9dcbf36228949df1c4f3920b0953d144b3437e0c
Jonathan Thom and Tracie Weitzman <jonathan+tracie@>
2017-02-22 11:09:14 -0800

    Set attributes before create

--- app/models/nation.rb
+++ app/models/nation.rb
@@ -4,4 +4,48 @@ class Nation < ActiveRecord::Base
   has_many :responses
   has_many :events, through: :responses
   validates :name, :government, :economy, :geography, :presence => true
+
+  before_create :set_attributes
+
+  def set_attributes
+    #stability
+    if self.government == 'Dictatorship'
+      self.stability = 0.25
+    elsif self.government == 'Democracy'
+      self.stability = 0.75
+    else
+      self.stability = 0.5
+    end
+
+    #capital
+    if self.economy == 'Capitalist'
+      self.capital = 1000
+    elsif self.economy == 'Socialist'
+      self.capital = 500
+    else
+      self.capital = 750
+    end
+
+    #population
+    if self.geography == 'Desert'
+      self.population = 10000
+    elsif self.geography == 'Forest'
+      self.population = 30000
+    elsif self.geography == 'Plains'
+      self.population = 20000
+    else
+      self.population = 5000
+    end
+
+    #resources
+    if self.geography == 'Forest' && self.economy == 'Capitalist'
+      self.resources = 1000
+    elsif self.geography == 'Mountains' && self.economy == 'Socialist'
+      self.resources = 250
+    else
+      self.resources = 700
+    end
+
+  end
+
 end
