73289936aeb4d5659f4373f112dc567de81753bc
Jonathan Thom and Tracie Weitzman <jonathan+tracie@>
2017-02-23 14:30:35 -0800

    Change backgrounds based on geography of nation

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -151,6 +151,7 @@ Devise.setup do |config|
   # Options to be passed to the created cookie. For instance, you can set
   # secure: true in order to force SSL only cookies.
   # config.rememberable_options = {}
+  config.scoped_views = true
 
   # ==> Configuration for :validatable
   # Range for password length.
