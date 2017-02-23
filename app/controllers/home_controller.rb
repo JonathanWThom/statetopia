6080d8ac683c4cae724810192282a204ad98f3dc
Jonathan Thom and Tracie Weitzman <jonathan+tracie@>
2017-02-23 13:46:42 -0800

    Fix log out

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @nations = current_user.nations.order('id desc').paginate(:per_page => 5, :page => params[:page])
+    if user_signed_in?
+      @nations = current_user.nations.order('id desc').paginate(:per_page => 5, :page => params[:page])
+    end
   end
 end
